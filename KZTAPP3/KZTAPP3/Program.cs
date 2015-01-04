36c2e3d7114c166103aaaf2ac9952154d4017956
dym_9FPC <308960474@qq.com>
2015-01-04 19:15:38 +0800

    update init

--- KZTAPP3/KZTAPP3/Program.cs
+++ KZTAPP3/KZTAPP3/Program.cs
@@ -10,10 +10,11 @@ namespace KZTAPP3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Everyone!");
-            Console.WriteLine("HAHAHAHA");
+            Console.WriteLine("11111111");
+            Console.WriteLine("22222222");
             Console.WriteLine("33333333");
             Console.WriteLine("44444444");
+            Console.WriteLine("66666666");
             Console.WriteLine("77777777");
         }
     }
