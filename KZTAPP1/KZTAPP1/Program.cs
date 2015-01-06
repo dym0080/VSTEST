53464920d14dab15cb9488beb4d8da7e234cd217
dym_ASUS <308960474@qq.com>
2015-01-06 11:10:43 +0800

    建议94

--- KZTAPP1/KZTAPP1/Program.cs
+++ KZTAPP1/KZTAPP1/Program.cs
@@ -10,12 +10,63 @@ namespace KZTAPP1
     {
         static void Main(string[] args)
         {
-            Diamod s = new Diamod();
-            s.MethodVirtual();
-            s.Method();
+            TestShape();
+            TestDerive();
+            TestDerive2();
             Console.ReadKey();
         }
+        private static void TestShape()
+        {
+            Console.WriteLine("TestShape\tStart");
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+            shapes.Add(new Triangle());
+            shapes.Add(new Diamod());
+            foreach (Shape s in shapes)
+            {
+                s.MethodVirtual();
+                s.Method();
+            }
+            Console.WriteLine("TestShape\tEnd");
+        }
+        private static void TestDerive()
+        {
+            Console.WriteLine("TestDerive\tStart");
+            Circle circle = new Circle();
+            Rectangle rectangle = new Rectangle();
+            Triangle triangle = new Triangle();
+            Diamod diamod = new Diamod();
+            circle.MethodVirtual();
+            circle.Method();
+            rectangle.MethodVirtual();
+            rectangle.Method();
+            triangle.MethodVirtual();
+            triangle.Method();
+            diamod.MethodVirtual();
+            diamod.Method();
+            Console.WriteLine("TestDerive\tEnd");
+        }
+        private static void TestDerive2()
+        {
+            Console.WriteLine("TestDerive2\tStart");
+            Circle circle = new Circle();
+            Rectangle rectangle = new Rectangle();
+            Triangle triangle = new Triangle();
+            Diamod diamod = new Diamod();
+            PrintShape(circle);
+            PrintShape(rectangle);
+            PrintShape(triangle);
+            PrintShape(diamod);
+            Console.WriteLine("TestDerive2\tEnd");
+        }
+        static void PrintShape(Shape shape)
+        {
+            shape.MethodVirtual();
+            shape.Method();
+        }
     }
+
     public class Shape
     {
         public virtual void MethodVirtual()
