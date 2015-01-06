180a99d4f66c221a66ab92dba8710a765575c2c7
dym_ASUS <308960474@qq.com>
2015-01-06 13:58:59 +0800

    建议95 AND add tempfile

--- KZTAPP1/KZTAPP1/Program.cs
+++ KZTAPP1/KZTAPP1/Program.cs
@@ -10,105 +10,36 @@ namespace KZTAPP1
     {
         static void Main(string[] args)
         {
-            TestShape();
-            TestDerive();
-            TestDerive2();
+            American american = new American();
             Console.ReadKey();
         }
-        private static void TestShape()
-        {
-            Console.WriteLine("TestShape\tStart");
-            List<Shape> shapes = new List<Shape>();
-            shapes.Add(new Circle());
-            shapes.Add(new Rectangle());
-            shapes.Add(new Triangle());
-            shapes.Add(new Diamod());
-            foreach (Shape s in shapes)
-            {
-                s.MethodVirtual();
-                s.Method();
-            }
-            Console.WriteLine("TestShape\tEnd");
-        }
-        private static void TestDerive()
-        {
-            Console.WriteLine("TestDerive\tStart");
-            Circle circle = new Circle();
-            Rectangle rectangle = new Rectangle();
-            Triangle triangle = new Triangle();
-            Diamod diamod = new Diamod();
-            circle.MethodVirtual();
-            circle.Method();
-            rectangle.MethodVirtual();
-            rectangle.Method();
-            triangle.MethodVirtual();
-            triangle.Method();
-            diamod.MethodVirtual();
-            diamod.Method();
-            Console.WriteLine("TestDerive\tEnd");
-        }
-        private static void TestDerive2()
-        {
-            Console.WriteLine("TestDerive2\tStart");
-            Circle circle = new Circle();
-            Rectangle rectangle = new Rectangle();
-            Triangle triangle = new Triangle();
-            Diamod diamod = new Diamod();
-            PrintShape(circle);
-            PrintShape(rectangle);
-            PrintShape(triangle);
-            PrintShape(diamod);
-            Console.WriteLine("TestDerive2\tEnd");
-        }
-        static void PrintShape(Shape shape)
-        {
-            shape.MethodVirtual();
-            shape.Method();
-        }
     }
+    class Person
+    {
+        public Person()
+        {
+            InitSkin();
+        }
+        protected virtual void InitSkin()
+        {
 
-    public class Shape
-    {
-        public virtual void MethodVirtual()
-        {
-            Console.WriteLine("base MethodVirtural call");
-        }
-        public void Method()
-        {
-            Console.WriteLine("base Method call");
         }
     }
-    public class Circle : Shape
+    class American:Person
     {
-        public override void MethodVirtual()
+        Race race;
+        public American()
+            :base()
         {
-            Console.WriteLine("Circle override MethodVirtual");
+            Race race = new Race() { Name = "White" };
+        }
+        protected override void InitSkin()
+        {
+            Console.WriteLine(race.Name);
         }
     }
-    public class Rectangle : Shape
+    class Race
     {
-
-    }
-    public class Triangle : Shape
-    {
-        public new void MethodVirtual()
-        {
-            Console.WriteLine("Triangle new MethodVirtual");
-        }
-        public new void Method()
-        {
-            Console.WriteLine("Triangle new Method");
-        }
-    }
-    public class Diamod : Shape
-    {
-        public void MethodVirtual()
-        {
-            Console.WriteLine("Diamod default MethodVirtual");
-        }
-        public void Method()
-        {
-            Console.WriteLine("Diamod default Method");
-        }
+        public string Name{get;set;}
     }
 }
