9dcaa56d8c7d85c6fc243580ec5f5ba03672722b
dym_ASUS <308960474@qq.com>
2015-01-14 16:48:30 +0800

    temp

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -10,13 +10,23 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(50,MinimumLength=3)]
         public string Title { get; set; }
 
         [Display(Name="Release Date"),DataType(DataType.Date)]
         [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}",ApplyFormatInEditMode=true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required,StringLength(40)]
         public string Genre { get; set; }
+
+        [Range(1,200),DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(6)]
         public string Rating { get; set; }
     }
     public class MovieDBContext:DbContext
