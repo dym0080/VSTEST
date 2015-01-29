900be88280bb370e0f9c997fe8cb00ddc71efe31
dym_ASUS <308960474@qq.com>
2015-01-29 22:11:05 +0800

    advice 15 of 157

--- KZTAPP3/KZTAPP3/Program.cs
+++ KZTAPP3/KZTAPP3/Program.cs
@@ -12,62 +12,26 @@ namespace KZTAPP3
     {
         static void Main(string[] args)
         {
-            Employee mike = new Employee()
-            {
-                IDCode = "NB123",
-                Age = 30,
-                Department = new Department() { Name = "Dep1" }
-            };
-            Employee rose = mike.Clone() as Employee;
-            Console.WriteLine(mike.IDCode);
-            Console.WriteLine(mike.Age);
-            Console.WriteLine(mike.Department);
-            Console.WriteLine("开始改变rose值：");
-            rose.IDCode = "NB456";
-            rose.Age = 60;
-            rose.Department.Name = "Dep2";
-            Console.WriteLine("mike ：");
-            Console.WriteLine(mike.IDCode);
-            Console.WriteLine(mike.Age);
-            Console.WriteLine(mike.Department);
-            Console.WriteLine("rose ：");
-            Console.WriteLine(rose.IDCode);
-            Console.WriteLine(rose.Age);
-            Console.WriteLine(rose.Department);
+            DynamicSample dynamicSample = new DynamicSample();
+
+            int res = dynamicSample.Add(1, 2);
+
+            Console.WriteLine(res);
             Console.ReadKey();
 
 
         }
     }
 
-    [Serializable]
-    class Employee : ICloneable
-    {
-        public string IDCode { get; set; }
-        public int Age { get; set; }
-        public Department Department { get; set; }
-
-        public object Clone()
-        {
-            using (Stream sr = new MemoryStream())
-            {
-                IFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(sr, this);
-                sr.Seek(0, SeekOrigin.Begin);
-                return formatter.Deserialize(sr) as Employee;
-            }
-        }
-
-    }
-    [Serializable]
-    class Department
+    public class DynamicSample
     {
         public string Name { get; set; }
-        public override string ToString()
+        public int Add(int a, int b)
         {
-            return this.Name;
+            return a + b;
         }
     }
+
 
 
 
