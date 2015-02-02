e31c2d8b7598d9a26387c41e1c534269a5aeb3f5
dym_9FPC <308960474@qq.com>
2015-02-02 16:41:32 +0800

    temp

--- KZTAPP3/KZTAPP3/Program.cs
+++ KZTAPP3/KZTAPP3/Program.cs
@@ -7,45 +7,27 @@ namespace KZTAPP3
 {
     class Program
     {
+        static Week week;
         static void Main(string[] args)
         {
-            Student student = new Student();
-            Console.WriteLine(student.GetSexString());
+            week = (Week)9;
+            Console.Write(week);
             Console.ReadKey();
+            
         }
     }
 
-    abstract class Stream
-    { }
-    class FileStream : Stream
-    { }
-    class MemoryStram : Stream
-    { }
-
-    
-    class Context
-    { }
-    class CultureInfo
-    { }
-    class Thread
+    enum Week
     {
-        private Context _context;
-        private CultureInfo _cultureInfo;
+        aa=0,
+        a=1,
+        b=2,
+        c=3,
+        d=4,
+        e=5,
+        f=6,
+        g=7
     }
 
-    public static class StudentConverter
-    {
-        public static string GetSexString(this Student student)
-        {
-            return student.GetSex() == true ? "男" : "女";
-        }
-    }
-    public class Student
-    {
-        public bool GetSex()
-        {
-            return false;
-        }
-    }
 
 }
