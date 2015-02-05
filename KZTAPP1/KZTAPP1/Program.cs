65b1147dcaa5afe1155d462be36b8ed2674af1ad
dym_9FPC <308960474@qq.com>
2015-02-05 10:38:43 +0800

    test

--- KZTAPP1/KZTAPP1/Program.cs
+++ KZTAPP1/KZTAPP1/Program.cs
@@ -15,23 +15,46 @@ namespace KZTAPP1
 
         static void Main(string[] args)
         {
-            List<Action> lists = new List<Action>();
-            for (int i = 0; i < 5; i++)
-            {
-                Action t=()=>
-                    {
-                        Console.WriteLine(i.ToString());
-                    };
-                lists.Add(t);
-            }
-            foreach (Action t in lists)
-            {
-                t();
-            }
+            FileUploader f1 = new FileUploader();
+            f1.FileUploaded += Progress;
+            f1.FileUploaded += AnotherProgess;
+            f1.Upload();
 
             Console.ReadKey();
         }
 
+        static void Progress(object sender,FileUploaderEventArgs e)
+        {
+            Console.WriteLine(e.FileProgress);
+        }
+        static void AnotherProgess(object sender,FileUploaderEventArgs e)
+        {
+            Console.WriteLine("另外一个Progress: "+ e.FileProgress);
+        }
+    }
+
+    class FileUploader
+    {
+        //public delegate void FileUploaderHandler(int progress);
+        //public event FileUploaderHandler FileUploaded;
+        public event EventHandler<FileUploaderEventArgs> FileUploaded;
+        public void Upload()
+        {
+            //int fileProgess = 10;
+            FileUploaderEventArgs e = new FileUploaderEventArgs() { FileProgress = 10 };
+            while(e.FileProgress>0)
+            {
+                e.FileProgress--;
+                if(FileUploaded!=null)
+                {
+                    FileUploaded(this,e);
+                }
+            }
+        }
+    }
+    class FileUploaderEventArgs:EventArgs
+    {
+        public int FileProgress { get; set; }
     }
 
 }
