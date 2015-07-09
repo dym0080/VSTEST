053a38eff578649e3ef8b5a3f513f7a9156e9686
dym0080 <308960474@qq.com>
2015-07-09 16:09:03 +0800

    test

--- KZTAPP1/KZTAPP1/Program.cs
+++ KZTAPP1/KZTAPP1/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Collections;
 using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace KZTAPP1
 {
@@ -16,23 +17,58 @@ namespace KZTAPP1
     {
         static void Main(string[] args)
         {
-
-            new Thread(Go).Start();
-            Go();
+            Person mike = new Person() { Age = 21, Name = "Mike" };
+            mike.NameChanged += new EventHandler(Mike_ddNameChanged);
+            SerializeNow(mike);
+            DeSerializeNow();
+            mike.Name = "Rose";
+            Console.WriteLine(mike.Name);
+            Console.WriteLine(mike.Age);
             Console.ReadKey();
-
         }
-
-        private static void Go()
+        static void Mike_ddNameChanged(object sender, EventArgs e)
         {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write("?");
-            }
+            Console.WriteLine("Name Changed");
         }
-       
+        public static void SerializeNow(Person person)
+        {
+            FileStream fileStream = new FileStream("E:\\temp11.txt", FileMode.Create);
+            BinaryFormatter b = new BinaryFormatter();
+            b.Serialize(fileStream, person);
+            fileStream.Close();
+        }
+        public static void DeSerializeNow()
+        {
+            Person person = new Person();
+            FileStream fileStream = new FileStream("E:\\temp11.txt",
+            FileMode.Open, FileAccess.Read, FileShare.Read);
+            BinaryFormatter b = new BinaryFormatter();
+            person = b.Deserialize(fileStream) as Person;
+            fileStream.Close();
+        }
 
     }
+
+    [Serializable]
+    class Person
+    {
+        private string name;
+        public int Age { get; set; }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (NameChanged != null)
+                {
+                    NameChanged(this, null);
+                }
+                name = value;
+            }
+        }
+        public event EventHandler NameChanged;
+    }
+
 
 }
 
