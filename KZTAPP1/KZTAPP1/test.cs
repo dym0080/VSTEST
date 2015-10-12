918600aaab0b4ba17aa998c391ceca81987fde73
dym0080 <308960474@qq.com>
2015-10-12 14:43:35 +0800

    test

---
+++ KZTAPP1/KZTAPP1/test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KZTAPP1
+{
+    public class test
+    {
+        public static int BatchInert(string connectionString, string desTable, DataTable dt, int batchSize = 500)
+        {
+            using (var sbc = new SqlBulkCopy(connectionString, SqlBulkCopyOptions.UseInternalTransaction)
+            {
+                BulkCopyTimeout = 300,
+                NotifyAfter = dt.Rows.Count,
+                BatchSize = batchSize,
+                DestinationTableName = desTable
+            })
+            {
+                foreach (DataColumn column in dt.Columns)
+                    sbc.ColumnMappings.Add(column.ColumnName, column.ColumnName);
+                sbc.WriteToServer(dt);
+            }
+
+            return dt.Rows.Count;
+        }
+    }
+}
