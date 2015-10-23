9af172db3fec46237888a7a5f26a6cbf33601d16
dym0080 <308960474@qq.com>
2015-10-23 16:47:50 +0800

    JSTEST

---
+++ JavaScript/JsTest/JsTest/Book/JavaScript.DOM高级程序设计/Charpt1/DYM-start.js
@@ -0,0 +1,36 @@
+﻿
+
+
+
+(function () {
+
+
+    if (!window.DYM) { window['DYM'] = {} };
+
+    function isCompatible(other) { };
+    window['DYM']['isCompatible'] = isCompatible;
+
+    function $() { };
+    window['DYM']['$'] = $;
+
+    function addEvent(node, type, listener) { };
+    window['DYM']['addEvent'] = addEvent;
+
+    function removeEvent(node, type, listener) { };
+    window['DYM']['removeEvent'] = removeEvent;
+
+    function getElementsByClassName(className, tag, parent) { };
+    window['DYM']['getElementsByClassName'] = getElementsByClassName;
+
+    function toggleDisplay(node, value) { };
+    window['DYM']['toggleDisplay'] = toggleDisplay;
+
+    function insertAfter(node, referenceNode) { };
+    window['DYM']['insertAfter'] = insertAfter;
+
+    function removeChildren(parent) { };
+    window['DYM']['removeChildren'] = removeChildren;
+
+    function prependChild(parent, newChild) { };
+    window['DYM']['prependChild'] = prependChild;
+})();
