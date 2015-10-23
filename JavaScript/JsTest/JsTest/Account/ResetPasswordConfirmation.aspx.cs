d3d531c6fbaf50bcbbaefcb84c242dc91e034993
dym0080 <308960474@qq.com>
2015-10-23 15:59:00 +0800

    JSTEST

---
+++ JavaScript/JsTest/JsTest/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace JsTest.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ JavaScript/JsTest/JsTest/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JsTest.Startup))]
+namespace JsTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
