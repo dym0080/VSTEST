6e0a80d91744652f49754069e6f27ef307b80283
dym0080 <308960474@qq.com>
2016-03-07 16:36:23 +0800

    Update jscommon.js

--- JavaScript/js封装/jscommon.js
+++ JavaScript/js封装/jscommon.js
@@ -3,8 +3,31 @@
 var isArray = function(obj) { 
 return Object.prototype.toString.call(obj) === '[object Array]'; 
 }
-
+/****************************************************************************/
 //判断是否是一个函数
 function isFunction(obj) {
    return Object.prototype.toString.call(obj)=== '[object Function]';
 }
+/****************************************************************************/
+    /**
+    * 获取指定函数的函数名称（用于兼容IE）
+    * @param {Function} fun 任意函数
+    */
+	function getFunctionName(fun) {
+	    if (fun.name !== undefined)
+	        return fun.name;
+	    var ret = fun.toString();
+	    ret = ret.substr('function '.length);
+	    ret = ret.substr(0, ret.indexOf('('));
+	    return ret;
+	}
+    
+    //测试
+    function fn1(){}
+    var fn2=function(){};
+    var fn3=function AAA(){};
+	console.log(getFunctionName(fn1));//输出fn1
+	console.log(getFunctionName(fn2));//输出空
+	console.log(getFunctionName(fn3));//输出AAA
+/****************************************************************************/	
+	
