82f865a3882e9568d67afa0c5803687f155ee457
dym0080 <308960474@qq.com>
2016-03-08 15:09:33 +0800

    Update jscommon.js

--- JavaScript/js封装/jscommon.js
+++ JavaScript/js封装/jscommon.js
@@ -30,4 +30,36 @@ function isFunction(obj) {
 	console.log(getFunctionName(fn2));//输出空
 	console.log(getFunctionName(fn3));//输出AAA
 /****************************************************************************/	
-	
+	/**
+	 * 查询指定窗口的视口尺寸，如果不指定窗口，查询当前窗口尺寸
+	 **/
+	function getViewportSize(w) {
+		w = w || window;
+
+		// IE9及标准浏览器中可使用此标准方法
+		if ('innerHeight' in w) {
+			return {
+				width: w.innerWidth,
+				height: w.innerHeight
+			};
+		}
+
+		var d = w.document;
+		// IE 8及以下浏览器在标准模式下
+		if (document.compatMode === 'CSS1Compat') {
+			return {
+				width: d.documentElement.clientWidth,
+				height: d.documentElement.clientHeight
+			};
+		}
+
+		// IE8及以下浏览器在怪癖模式下
+		return {
+			width: d.body.clientWidth,
+			height: d.body.clientHeight
+		};
+	}
+    
+        //test
+	console.log(getViewportSize());//Object {width: 1280, height: 214}	
+/****************************************************************************/
