e40c98191ad846985367ec9850a24e1bcbb0fedf
dym0080 <308960474@qq.com>
2016-03-16 16:57:27 +0800

    test

--- JavaScript/StTest/test.js
+++ JavaScript/StTest/test.js
@@ -53,6 +53,20 @@
 		},
 		getTarget:function(event){
 			return event.target||event.srcElement;
+		},
+		preventDefault:function(event){
+			if(event.preventDefault){
+				event.preventDefault();
+			}else{
+				event.returnValue=false;
+			}
+		},
+		stopPropagation:function(event){
+			if(event.stopPropagation){
+				event.stopPropagation();
+			}else{
+				event.cancelBubble=true;
+			}
 		}
 	};
 
