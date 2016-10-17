a69bc3ce4b39dc73e3fdba176bcba55f351666a6
dym <308960474@qq.com>
2016-10-17 15:08:58 +0800

    test

--- KZTAPP1/KZTAPP1/Program.cs
+++ KZTAPP1/KZTAPP1/Program.cs
@@ -16,64 +16,81 @@ namespace KZTAPP1
 
     public class Program
     {
-        private static readonly Stopwatch Watch = new Stopwatch();
         public static void Main()
-        {
-            //启动计时器
-            Watch.Start();
-
-            const string url1 = "http://www.cnblogs.com/";
-            const string url2 = "http://www.cnblogs.com/liqingwen/";
-
-            //两次调用 CountCharacters 方法（下载某网站内容，并统计字符的个数）
-            Task<int> t1 = CountCharacters(1, url1);
-            Task<int> t2 = CountCharacters(2, url2);
-
-            //三次调用 ExtraOperation 方法（主要是通过拼接字符串达到耗时操作）
-            for (var i = 0; i < 3; i++)
-            {
-                ExtraOperation(i + 1);
-            }
-
-            //控制台输出
-            Console.WriteLine($"{url1} 的字符个数：{t1}");
-            Console.WriteLine($"{url2} 的字符个数：{t2}");
-
-            Console.Read();
+        {//Animal的实例
+            Animal a = new Animal();
+            //Animal的实例，引用派生类Cat对象
+            Animal ac = new Cat();
+            //Animal的实例，引用派生类Dog对象
+            Animal ad = new Dog();
+            //Cat的实例
+            Cat c = new Cat();
+            //Dog的实例
+            Dog d = new Dog();
+            //重载
+            a.Sleep();//Animal Sleep
+            a.Sleep(23);//Animal在23点Sleep
+            //重写和虚方法
+            a.EatFood();//Animal EatFood
+            ac.EatFood();//Cat EatFood
+            ad.EatFood();//Dog EatFood
+            //抽象方法
+            a.Live();//Animal重写的Live
+            //隐藏方法
+            a.Sleep();//Animal Sleep
+            ac.Sleep();//Cat Sleep
+            c.Sleep();//Cat Sleep
+            Console.ReadKey();
         }
 
-        /// <summary>
-        /// 统计字符个数
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="address"></param>
-        /// <returns></returns>
-        private static async Task<int> CountCharacters(int id, string address)
+        public abstract class Biology
         {
-            var wc = new WebClient();
-            Console.WriteLine($"开始调用 id = {id}：{Watch.ElapsedMilliseconds} ms");
-
-            var result =await wc.DownloadStringTaskAsync(address);
-            Console.WriteLine($"调用完成 id = {id}：{Watch.ElapsedMilliseconds} ms");
-
-            return result.Length;
+            public abstract void Live();
         }
-
-        /// <summary>
-        /// 额外操作
-        /// </summary>
-        /// <param name="id"></param>
-        private static void ExtraOperation(int id)
+        public class Animal : Biology
         {
-            //这里是通过拼接字符串进行一些相对耗时的操作
-            var s = "";
-
-            for (var i = 0; i < 6000; i++)
+            public override void Live()
             {
-                s += i;
+                Console.WriteLine("Animal重写的Live");
+                //throw new NotImplementedException();
             }
-
-            Console.WriteLine($"id = {id} 的 ExtraOperation 方法完成：{Watch.ElapsedMilliseconds} ms");
+            public void Sleep()
+            {
+                Console.WriteLine("Animal Sleep");
+            }
+            public int Sleep(int time)
+            {
+                Console.WriteLine("Animal在{0}点Sleep", time);
+                return time;
+            }
+            public virtual void EatFood()
+            {
+                Console.WriteLine("Animal EatFood");
+            }
+        }
+        public class Cat : Animal
+        {
+            public override void EatFood()
+            {
+                Console.WriteLine("Cat EatFood");
+                //base.EatFood();
+            }
+            new public void Sleep()
+            {
+                Console.WriteLine("Cat Sleep");
+            }
+            //public new void Sleep()
+            //{
+            //    Console.WriteLine("Cat Sleep");
+            //}
+        }
+        public class Dog : Animal
+        {
+            public override void EatFood()
+            {
+                Console.WriteLine("Dog EatFood");
+                //base.EatFood();
+            }
         }
 
     }
